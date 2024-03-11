d99f9f3ebb690e2ae732205fe1d8a760f8c7974e
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-03-11 22:59:02 +0200

    rename

--- createCourse.cpp
+++ createCourse.cpp
@@ -139,7 +139,7 @@ ErrorCode createCourse(const string &courseName, const string &courseCategory, c
     if (courseName.length() > 100 || courseCategory.length() > 100 || courseDescription.length() > 1000)
     {
         return ERROR_009;
-    }
+    }   
     // Pārbauda attēla formātu
     if (previewImage.format != "JPEG" && previewImage.format != "JPG" && previewImage.format != "PNG")
     {
@@ -155,7 +155,7 @@ ErrorCode createCourse(const string &courseName, const string &courseCategory, c
 
 /// TESTĒŠANAS FUNKCIJA DATU PĀRBAUDĒM (TEST CASES)
 
-void testCreateCourse(int testCaseID, ErrorCode expectedResult, string courseName, string categoryName, Image image, string description, string price) {
+void testCreateCourse(string testCaseID, ErrorCode expectedResult, string courseName, string categoryName, Image image, string description, string price) {
     ErrorCode result = createCourse(courseName, categoryName, image, description, price);
     if (result == expectedResult)
     {
@@ -170,20 +170,20 @@ void testCreateCourse(int testCaseID, ErrorCode expectedResult, string courseNam
 void functionTesting()
 {
     // Valid function inputs
-    string validCourseName = "course 1";
-    string validCategoryName = "category 1";
+    string validCourseName = "Kurss 1";
+    string validCategoryName = "Kategorija 1";
     Image validImage = {"JPEG", 1024 * 1024};
-    string validDescription = "This is a valid description";
+    string validDescription = "Šis ir apraksts";
     string validPrice = "10.00";
     
     // Testing Variable definition
     string emptyString = "";
     string hundredSymbolString = "PK9fpGN0MH#M.UjiE7X4#[k8AqdeVpQ&w5}*76BqQA5A*%]bcN@cP;jqv*:1f]cGj4RF[yFzx{K$L[3[chL0QAT!MpMg(:n+!*$h";
-    string moreThanHundredSymbolString = "PK9fpGN0MH#M.UjiE7X4#[k8AqdeVpQ&w5}*76BqQA5A*%]bcN@cP;jqv*:1f]cGj4RF[yFzx{K$L[3[chL0QAT!MpMg(:n+!*$hqsssda";
+    string oneHundredOneSymString = "PK9fpGN0MH#M.UjiE7X4#[k8AqdeVpQ&w5}*76BqQA5A*%]bcN@cP;jqv*:1f]cGj4RF[yFzx{K$L[3[chL0QAT!MpMg(:n+!*$he";
     
     string thousandSymbolString = "=bRDGzZSALt/H9GmbkhiRm-HQDv{L1wc#x[0!0wThG}G4GY#{*-;xjUa}%E(_&g&GF].p-[YQN3p{4w1J).]KQCe.9kZR0]U1)6-AjvM8p6X!m.vw%FWG*@2$uP/*cP0mQ$)dnGk:D;DH6iyLN@UpQ1[tM8N,1$RR&Ck39Nk}R1V57/W+1pmUqfAiEgCLqW#+!rt*{mtAgJmmD){{5kktwtW+S:Qb&yNQMDcVfQ14f/(n9!w5JUx4kT@H:bgd(6apARPgRg-vqcY$+_!deMLF]k[zg!1?])N%P8C9ubGQ(2!d),{K4!m&=Lwv3dFy!]Ej{4pGvmR&NZt%]2XF3k=PL@!13Zx?B)?LyXe2ppZfhEKQmk*JyC5*)W:MV8@qR8.MTfv]VRw@eVyV@NBrXKE=0F#VF6Uzwu:GD@[3[T_VB:8q6U4R0()F)jN0AB9ULynck@DraS*LVLME3cGL&2SE4?uU51qFa+#d6/($%F9hf*yb!1,8ae]CDWcDU];,8q{.fw51Hi)7DNrx!83pb7/=ivN3w=#_2*$[a4[:{){*:Fpk=#rb)b,+BAj8Ga4jbE3%U*[QM&r#Rj9PT%nGS/b=agjaDak;Zb;iq=5LaXS5B%Q2j[Mg?JgXbMRK3#210{W0.!Vw5krddEn2H+/hX4_fg5MyihrSgBiU/gv_?Z9P!L5VeVH]Q+G}{.d)qzBhR!29EjkuY;0{6kE)J.wbt4SFH;@h1M6bY6Z9?7{=m!0Y}by$vn{wjUH11BN**k:E=8wZ{H,6U0k[H+@9w)RGWFHKM)KE7C#g}7f=b?{SMSw4[F.h0v)T%2HZH(+6(.,ZcMiHcH4P1{+W*w[!xGeiphVKVNR#0S?4gt5irv.pU=!gpYaQqW=4gJa*K#?N[K7:97dZ.i;!dQ3p:]kZNc2-HK;2.F0Pf;c1,khH&vR(%x0gmt!eh&wV%_6F%ny{kD+WRRaDX$_WU)3*6M&gbU,!Nm5g#C34MvFwvWg+hkxqcK#";
     
-    string moreThanThousandSymbolString = "=bRDGzZSALt/H9GmbkhiRm-HQDv{L1wc#x[0!0wThG}G4GY#{*-;xjUa}%E(_&g&GF].p-[YQN3p{4w1J).]KQCe.9kZR0]U1)6-AjvM8p6X!m.vw%FWG*@2$uP/*cP0mQ$)dnGk:D;DH6iyLN@UpQ1[tM8N,1$RR&Ck39Nk}R1V57/W+1pmUqfAiEgCLqW#+!rt*{mtAgJmmD){{5kktwtW+S:Qb&yNQMDcVfQ14f/(n9!w5JUx4kT@H:bgd(6apARPgRg-vqcY$+_!deMLF]k[zg!1?])N%P8C9ubGQ(2!d),{K4!m&=Lwv3dFy!]Ej{4pGvmR&NZt%]2XF3k=PL@!13Zx?B)?LyXe2ppZfhEKQmk*JyC5*)W:MV8@qR8.MTfv]VRw@eVyV@NBrXKE=0F#VF6Uzwu:GD@[3[T_VB:8q6U4R0()F)jN0AB9ULynck@DraS*LVLME3cGL&2SE4?uU51qFa+#d6/($%F9hf*yb!1,8ae]CDWcDU];,8q{.fw51Hi)7DNrx!83pb7/=ivN3w=#_2*$[a4[:{){*:Fpk=#rb)b,+BAj8Ga4jbE3%U*[QM&r#Rj9PT%nGS/b=agjaDak;Zb;iq=5LaXS5B%Q2j[Mg?JgXbMRK3#210{W0.!Vw5krddEn2H+/hX4_fg5MyihrSgBiU/gv_?Z9P!L5VeVH]Q+G}{.d)qzBhR!29EjkuY;0{6kE)J.wbt4SFH;@h1M6bY6Z9?7{=m!0Y}by$vn{wjUH11BN**k:E=8wZ{H,6U0k[H+@9w)RGWFHKM)KE7C#g}7f=b?{SMSw4[F.h0v)T%2HZH(+6(.,ZcMiHcH4P1{+W*w[!xGeiphVKVNR#0S?4gt5irv.pU=!gpYaQqW=4gJa*K#?N[K7:97dZ.i;!dQ3p:]kZNc2-HK;2.F0Pf;c1,khH&vR(%x0gmt!eh&wV%_6F%ny{kD+WRRaDX$_WU)3*6M&gbU,!Nm5g#C34MvFwvWg+hkxqcK#sdgsd";
+    string oneThousandOneSymString = "=bRDGzZSALt/H9GmbkhiRm-HQDv{L1wc#x[0!0wThG}G4GY#{*-;xjUa}%E(_&g&GF].p-[YQN3p{4w1J).]KQCe.9kZR0]U1)6-AjvM8p6X!m.vw%FWG*@2$uP/*cP0mQ$)dnGk:D;DH6iyLN@UpQ1[tM8N,1$RR&Ck39Nk}R1V57/W+1pmUqfAiEgCLqW#+!rt*{mtAgJmmD){{5kktwtW+S:Qb&yNQMDcVfQ14f/(n9!w5JUx4kT@H:bgd(6apARPgRg-vqcY$+_!deMLF]k[zg!1?])N%P8C9ubGQ(2!d),{K4!m&=Lwv3dFy!]Ej{4pGvmR&NZt%]2XF3k=PL@!13Zx?B)?LyXe2ppZfhEKQmk*JyC5*)W:MV8@qR8.MTfv]VRw@eVyV@NBrXKE=0F#VF6Uzwu:GD@[3[T_VB:8q6U4R0()F)jN0AB9ULynck@DraS*LVLME3cGL&2SE4?uU51qFa+#d6/($%F9hf*yb!1,8ae]CDWcDU];,8q{.fw51Hi)7DNrx!83pb7/=ivN3w=#_2*$[a4[:{){*:Fpk=#rb)b,+BAj8Ga4jbE3%U*[QM&r#Rj9PT%nGS/b=agjaDak;Zb;iq=5LaXS5B%Q2j[Mg?JgXbMRK3#210{W0.!Vw5krddEn2H+/hX4_fg5MyihrSgBiU/gv_?Z9P!L5VeVH]Q+G}{.d)qzBhR!29EjkuY;0{6kE)J.wbt4SFH;@h1M6bY6Z9?7{=m!0Y}by$vn{wjUH11BN**k:E=8wZ{H,6U0k[H+@9w)RGWFHKM)KE7C#g}7f=b?{SMSw4[F.h0v)T%2HZH(+6(.,ZcMiHcH4P1{+W*w[!xGeiphVKVNR#0S?4gt5irv.pU=!gpYaQqW=4gJa*K#?N[K7:97dZ.i;!dQ3p:]kZNc2-HK;2.F0Pf;c1,khH&vR(%x0gmt!eh&wV%_6F%ny{kD+WRRaDX$_WU)3*6M&gbU,!Nm5g#C34MvFwvWg+hkxqcK#f";
 
     Image JPEGImage = {"JPEG", 1024 * 1024};
     Image PNGImage = {"PNG", 1024 * 1024};
@@ -192,46 +192,49 @@ void functionTesting()
     Image ABCDImage = {"ABCD", 1024 * 1024};
     Image size0mbImage = {"JPG", 0};
     Image size3mbImage = {"JPEG", 1024 * 1024 * 3};
-    Image moreThan3mbImage = {"JPEG", 1024 * 1024 * 30};
+    Image moreThan3mbImage = {"JPEG", 1024 * 1024 * 4};
 
     string negativePrice = "-10";
     string zeroPrice = "0";
     string maxPrice = "9999.99";
+    string multipleDotPrice = "10..0";
 
     // courseName TESTS
-    testCreateCourse(1, ERROR_001, emptyString, validCategoryName, validImage, validDescription, validPrice);
-    testCreateCourse(2, SUCCESS, validCourseName, validCategoryName, validImage, validDescription, validPrice);
-    testCreateCourse(3, SUCCESS, hundredSymbolString, validCategoryName, validImage, validDescription, validPrice);
-    testCreateCourse(4, ERROR_009, moreThanHundredSymbolString, validCategoryName, validImage, validDescription, validPrice);
+    testCreateCourse("T_01", SUCCESS, validCourseName, validCategoryName, validImage, validDescription, validPrice);
+    testCreateCourse("T_02", ERROR_001, emptyString, validCategoryName, validImage, validDescription, validPrice);
+    testCreateCourse("T_03", SUCCESS, hundredSymbolString, validCategoryName, validImage, validDescription, validPrice);
+    testCreateCourse("T_04", ERROR_009, oneHundredOneSymString, validCategoryName, validImage, validDescription, validPrice);
     // courseName TESTS END
 
     // courseCategory TESTS
-    testCreateCourse(5, ERROR_001, validCourseName, emptyString, validImage, validDescription, validPrice);
-    testCreateCourse(6, SUCCESS, validCourseName, hundredSymbolString, validImage, validDescription, validPrice);
-    testCreateCourse(7, ERROR_009, validCourseName, moreThanHundredSymbolString, validImage, validDescription, validPrice);
+    testCreateCourse("T_05", ERROR_001, validCourseName, emptyString, validImage, validDescription, validPrice);
+    testCreateCourse("T_06", SUCCESS, validCourseName, hundredSymbolString, validImage, validDescription, validPrice);
+    testCreateCourse("T_07", ERROR_009, validCourseName, oneHundredOneSymString, validImage, validDescription, validPrice);
     // courseDescription TESTS END
 
     // Image TESTS
-    testCreateCourse(8, SUCCESS, validCourseName, validDescription, JPEGImage, validDescription, validPrice);
-    testCreateCourse(9, SUCCESS, validCourseName, validDescription, PNGImage, validDescription, validPrice);
-    testCreateCourse(10, SUCCESS, validCourseName, validDescription, JPGImage, validDescription, validPrice);
-    testCreateCourse(11, ERROR_011, validCourseName, validDescription, TIFFImage, validDescription, validPrice);
-    testCreateCourse(12, ERROR_011, validCourseName, validDescription, ABCDImage, validDescription, validPrice);
-    testCreateCourse(13, SUCCESS, validCourseName, validDescription, size0mbImage, validDescription, validPrice);
-    testCreateCourse(14, SUCCESS, validCourseName, validDescription, size3mbImage, validDescription, validPrice);
-    testCreateCourse(15, ERROR_004, validCourseName, validDescription, moreThan3mbImage, validDescription, validPrice);
+    testCreateCourse("T_08", SUCCESS, validCourseName, validDescription, JPEGImage, validDescription, validPrice);
+    testCreateCourse("T_09", SUCCESS, validCourseName, validDescription, PNGImage, validDescription, validPrice);
+    testCreateCourse("T_10", SUCCESS, validCourseName, validDescription, JPGImage, validDescription, validPrice);
+    testCreateCourse("T_11", ERROR_011, validCourseName, validDescription, TIFFImage, validDescription, validPrice);
+    testCreateCourse("T_12", ERROR_011, validCourseName, validDescription, ABCDImage, validDescription, validPrice);
+    testCreateCourse("T_13", ERROR_002, validCourseName, validDescription, size0mbImage, validDescription, validPrice);
+    testCreateCourse("T_14", SUCCESS, validCourseName, validDescription, size3mbImage, validDescription, validPrice);
+    testCreateCourse("T_15", ERROR_004, validCourseName, validDescription, moreThan3mbImage, validDescription, validPrice);
     // Image TESTS END
 
     // courseDescription TESTS
-    testCreateCourse(16, ERROR_001, validCourseName, validCategoryName, validImage, emptyString, validPrice);
-    testCreateCourse(17, SUCCESS, validCourseName, validCategoryName, validImage, thousandSymbolString, validPrice);
-    testCreateCourse(18, ERROR_009, validCourseName, validCategoryName, validImage, moreThanThousandSymbolString, validPrice);
+    testCreateCourse("T_16", ERROR_001, validCourseName, validCategoryName, validImage, emptyString, validPrice);
+    testCreateCourse("T_17", SUCCESS, validCourseName, validCategoryName, validImage, thousandSymbolString, validPrice);
+    testCreateCourse("T_18", ERROR_009, validCourseName, validCategoryName, validImage, oneThousandOneSymString, validPrice);
     // courseDescription END
 
     // coursePrice TESTS
-    testCreateCourse(19, ERROR_002, validCourseName, validCategoryName, validImage, validDescription, negativePrice);
-    testCreateCourse(20, SUCCESS, validCourseName, validCategoryName, validImage, validDescription, maxPrice);
-    testCreateCourse(21, ERROR_002, validCourseName, validCategoryName, validImage, validDescription, hundredSymbolString);
+    testCreateCourse("T_19", ERROR_002, validCourseName, validCategoryName, validImage, validDescription, negativePrice);
+    testCreateCourse("T_20", SUCCESS, validCourseName, validCategoryName, validImage, validDescription, maxPrice);
+    testCreateCourse("T_21", ERROR_002, validCourseName, validCategoryName, validImage, validDescription, hundredSymbolString);
+    testCreateCourse("T_22", ERROR_002, validCourseName, validCategoryName, validImage, validDescription, zeroPrice);
+    testCreateCourse("T_23", ERROR_002, validCourseName, validCategoryName, validImage, validDescription, multipleDotPrice);
 }
 
 int main()
